b5fb3b3190d28dcc644a7bf48bbd6468c33dc852
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-08-31 15:43:25 +0700

    finish navbar lg screen styling

--- src/layouts/TheNavbar.vue
+++ src/layouts/TheNavbar.vue
@@ -9,25 +9,56 @@
             <img src="../assets/brand.jpg" width="250">
         </q-toolbar-title>
 
-        <q-btn icon="menu" class="lt-md no-shadow">
+        <div class="row lt-md gt-xs">
+            <div class="col-6">
+                <a href="https://twitter.com/salvia__mama" class="twitter social" target="_blank">
+                    <q-img src="../assets/twitter-logo-blue.png" width="50px" class="img"></q-img>
+                </a>
+            </div>
+            <div class="col-6">
+                <a href="https://www.instagram.com/salvia_mama/" class="instagram social" target="_blank">
+                    <q-img src="../assets/instagram-logo.png" width="45px" class="img"></q-img>
+                </a>
+            </div>
+        </div>
+
+        <q-btn icon="menu" class="lt-md no-shadow q-ml-xl">
             <q-menu auto-close>
+                <q-list style="min-width: 300px" class="lt-sm q-mt-md q-mb-md">
+                    <div class="row lt-md">
+                        <div class="col-6 column items-center">
+                            <a href="https://twitter.com/salvia__mama" target="_blank">
+                                <q-img src="../assets/twitter-logo-blue.png" width="37px"></q-img>
+                            </a>
+                        </div>
+                        <div class="col-6 column items-center">
+                            <a href="https://www.instagram.com/salvia_mama/" target="_blank">
+                                <q-img src="../assets/instagram-logo.png" width="33px"></q-img>
+                            </a>
+                        </div>
+                    </div>
+                </q-list>
+                <q-separator inset />
                 <q-list style="min-width: 300px" >
-                    <q-item clickable>
+                    <q-item clickable >
                     <q-item-section>レポラマとは</q-item-section>
                     </q-item>
-                    <q-item clickable>
+                    <q-item clickable >
                     <q-item-section>レポラマ紹介</q-item-section>
                     </q-item>
-                    <q-item clickable>
+                    <q-item clickable >
                     <q-item-section>レポの流れ</q-item-section>
                     </q-item>
-                    <q-item clickable>
+                    <q-item clickable >
                     <q-item-section>価格</q-item-section>
                     </q-item>
                     <q-item clickable>
+                    <q-item-section>その他</q-item-section>
+                    </q-item>
+                    <q-item clickable >
                     <q-item-section>よくある質問</q-item-section>
                     </q-item>
-                    <q-item clickable>
+                    <q-item clickable >
                     <q-item-section>お問い合わせ</q-item-section>
                     </q-item>
                 </q-list>
@@ -35,13 +66,13 @@
         </q-btn>
 
         <q-tabs align="right" class="gt-sm">
-            <q-route-tab to="#" label="レポラマとは" />
-            <q-route-tab to="#" label="レポラマ紹介" />
-            <q-route-tab to="#" label="レポの流れ" />
-            <q-route-tab to="#" label="価格" />
-            <q-route-tab to="#" label="その他" />
-            <q-route-tab to="#" label="よくある質問" />
-            <q-route-tab to="#" label="お問い合わせ" />
+            <a class="nav">レポラマとは</a>
+            <a class="nav">レポラマ紹介</a>
+            <a class="nav">レポの流れ</a>
+            <a class="nav">価格</a>
+            <a class="nav">その他</a>
+            <a class="nav">よくある質問</a>
+            <a class="nav">お問い合わせ</a>
         </q-tabs>
       </q-toolbar>
 
@@ -56,5 +87,26 @@ export default {
 </script>
 
 <style scoped>
+.twitter:hover{
+    background-color: white;
+}
+.instagram:hover{
+    background-color: white;
+}
+.social{
+    padding: 5px;
+    border-radius: 5px;
+    margin-right: 40px;
+}
 
+.nav:hover {
+    color: rgb(205, 75, 128);
+    text-decoration: underline rgb(205, 75, 128);
+    text-underline-position: under;
+}
+.nav{
+    cursor: pointer;
+    margin-right: 5em;
+    font-size: 1rem;
+}
 </style>
