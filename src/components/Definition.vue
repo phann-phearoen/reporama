5b6151334c6ea691907ca086b15ea14a8636bba7
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-01 13:49:41 +0700

    add definition section レポラマとは

---
+++ src/components/Definition.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row badger">
+        <base-badge :label="'レポラマとは'" :color="'rgb(205, 75, 128)'" :width="'70%'"></base-badge>
+    </div>
+</template>
+
+<script>
+import BaseBadge from './base/BaseBadge.vue';
+
+export default {
+    components: { BaseBadge, },
+}
+</script>
+
+<style scoped>
+.badger {
+    margin: 3vw auto;
+}
+</style>
--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -2,14 +2,16 @@
   
   <the-top></the-top>
 
+  <definition></definition>
+
 </template>
 
 <script>
 import TheTop from '../components/TheTop.vue';
+import Definition from '../components/Definition.vue';
+
 export default {
-  components: { TheTop, },
-  setup() {
-    
-  },
+  components: { TheTop, Definition, },
+  
 }
 </script>
