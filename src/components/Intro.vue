712ed140514980cd2bde4206fcf4aaa0b60bd363
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-01 17:25:21 +0700

    add all cards to intro section

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,14 +1,112 @@
 <template>
     <base-title :title="'記事例'"></base-title>
+    <div class="row contain">
+        <base-card
+        class="col-xl-3 col-lg-4"
+        v-for="(info, index) in introInfo"
+        :key="index"
+        :link="info.link"
+        :src="info.src"
+        :des="info.des"
+        >
+        
+        </base-card>
+    </div>
+
     <div class="row">
         <div class="col" style="height: 500px"></div>
     </div>
+    
 </template>
 
 <script>
 import BaseTitle from './base/BaseTitle.vue';
+import BaseCard from './base/BaseCard.vue';
 
 export default {
-    components: { BaseTitle, }    
+    components: { BaseTitle, BaseCard, },
+    data() {
+        return {
+            introInfo: [
+                { 
+                    src: require('../assets/articles/article_1.png'), 
+                    link: 'https://wg-salvia.com/archives/30591',
+                    des: {
+                        line1: 'クリスマス向け︕',
+                        line2: '簡単テーブルナプキンの',
+                        line3: '折り方を「MYdrap」で'
+                    }
+                },
+                { 
+                    src: require('../assets/articles/article_2.png'),
+                    link: 'https://wg-salvia.com/archives/29626',
+                    des: {
+                        line1: '★美白＆毛穴対策にも︕',
+                        line2: 'イエスパのシートマスク',
+                        line3: '＆パックがすごい！'
+                    }
+                },
+                { 
+                    src: require('../assets/articles/article_3.png'),
+                    link: 'https://wg-salvia.com/archives/29548',
+                    des: {
+                        line1: '20 分で簡単なめらかアイス♪',
+                        line2: '貝印のアイスクリームメーカー',
+                        line3: '＆レシピ'
+                    }
+                },
+                { 
+                    src: require('../assets/articles/article_4.png'),
+                    link: 'https://wg-salvia.com/archives/31239',
+                    des: {
+                        line1: '★「KOH」パールが輝く',
+                        line2: '　大人可愛い腕時計︕',
+                        line3: 'プレゼントにもおすすめ'
+                    }
+                },
+                { 
+                    src: require('../assets/articles/article_5.png'),
+                    link: 'https://wg-salvia.com/archives/27991',
+                    des: {
+                        line1: '★やまやの「めんたいチューブ」が',
+                        line2: '美味しくて便利︕簡単レシピも♪'
+                    }
+                },
+                { 
+                    src: require('../assets/articles/article_6.png'),
+                    link: 'https://wg-salvia.com/archives/30573',
+                    des: {
+                        line1: '寝起きの 1 分でパワーチャージ︕',
+                        line2: 'プロに聞く瞑想のやり方︕'
+                    }
+                },
+                { 
+                    src: require('../assets/articles/article_7.png'),
+                    link: 'https://wg-salvia.com/archives/22250',
+                    des: {
+                        line1: '★粘土クリエイター !?',
+                        line2: '小学生がおちゃっぴのねんど',
+                        line3: '「ピカリ」を体験'
+                    }
+                },
+                { 
+                    src: require('../assets/articles/article_8.png'),
+                    link: 'https://wg-salvia.com/archives/28324',
+                    des: {
+                        line1: '子連れ OK ︕',
+                        line2: '横浜の次世代サロン',
+                        line3: '「HIKARINO」でエステ体験取材'
+                    }
+                },
+            ]
+        }
+    }
 }
 </script>
+
+<style scoped>
+.contain{
+    width: 95%;
+    margin: 0 auto;
+}
+</style>
---
+++ src/components/base/BaseCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <a :href="link" class="card" target="_blank">
+        <q-card class="my-card">
+            <img :src="src">
+
+            <q-card-section class="q-pt-none">
+                <ul class="des">
+                    <li v-for="line in des" :key="line">
+                        {{ line }}
+                    </li>
+                </ul>
+            </q-card-section>
+        </q-card>
+    </a>
+</template>
+
+<script>
+
+export default {
+    props: ['src', 'link', 'des']
+}
+</script>
+
+<style scoped>
+.card{
+    cursor: pointer;
+    padding: 1vw;
+    text-decoration: none;
+}
+.des{
+    list-style-type: none;
+    font-size: 1vw;
+    margin-left: -.5vw;
+    color: grey;
+}
+</style>
