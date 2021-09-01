fb6bb418dbdec6d8b681e3a228aedb4855054fdf
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-01 16:14:51 +0700

    add intro to home

---
+++ src/components/Intro.vue
@@ -0,0 +1,14 @@
+<template>
+    <base-title :title="'記事例'"></base-title>
+    <div class="row">
+        <div class="col" style="height: 500px"></div>
+    </div>
+</template>
+
+<script>
+import BaseTitle from './base/BaseTitle.vue';
+
+export default {
+    components: { BaseTitle, }    
+}
+</script>
--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -4,14 +4,17 @@
 
   <definition></definition>
 
+  <intro></intro>
+
 </template>
 
 <script>
 import TheTop from '../components/TheTop.vue';
 import Definition from '../components/Definition.vue';
+import Intro from '../components/Intro.vue';
 
 export default {
-  components: { TheTop, Definition, },
+  components: { TheTop, Definition, Intro, },
   
 }
 </script>
