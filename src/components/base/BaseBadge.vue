82874c0a51e8ae092db208cc0c26992e43f79784
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-01 13:48:00 +0700

    create base-badge

---
+++ src/components/base/BaseBadge.vue
@@ -0,0 +1,26 @@
+<template>
+    <q-badge rounded :style="{ backgroundColor: color, width: width }" class="text">
+        <div class="label">{{ label }}</div>
+    </q-badge>
+</template>
+
+<script>
+export default {
+    props: ['label', 'color', 'width'],
+}
+</script>
+
+<style scoped>
+.text {
+    padding: 1.4vw;
+    margin-left: auto;
+    margin-right: auto;
+    color: white;
+    font-size: 2vw;
+    font-weight: 600;
+}
+.label {
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
