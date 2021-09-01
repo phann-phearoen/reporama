d4e81315874c4a8544262f2723ae1bf17e4456f8
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-01 16:06:41 +0700

    add bast title

---
+++ src/components/base/BaseTitle.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="row">
+        <div class="pinkey"><hr class="pinky-line"></div>
+        <div class="title">{{ title }}</div>
+        <div class="pinkey"><hr class="pinky-line"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title', ]
+}
+</script>
+
+<style scoped>
+.row{
+    width: 95%;
+    margin: 2vw auto;
+}
+.pinky-line{
+    height: 3px;
+    background-color: rgb(205, 75, 128);
+    border: none;
+    width: 90%;
+    margin-top: 1.4vw;
+}
+.pinkey{
+    flex-grow: 1;
+}
+.title{
+    margin: 0 auto;
+    font-size: 2vw;
+    color: rgb(205, 75, 128);
+    font-weight: 600;
+}
+</style>
