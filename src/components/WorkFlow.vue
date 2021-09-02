baec95c925ef6eac0a54e1c0fcafd506ffba1365
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-02 15:56:41 +0700

    add WorkFlow component

--- src/components/WorkFlow.vue
+++ src/components/WorkFlow.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Workflow</h1>
+<div class="contain">
+    <div class="row">
+        <base-badge :label="'レポラマの流れ'" :color="'rgb(205, 75, 128)'" :width="'70%'" :pad="'1.4vw'"></base-badge>
+    </div>
+    <div class="row flow">
+        <q-img src="../assets/flow.png"></q-img>
+    </div>
+
+</div>
+
 </template>
+
+<script>
+import BaseBadge from './base/BaseBadge.vue';
+
+export default {
+    components: { BaseBadge, }
+    
+}
+</script>
+
+<style scoped>
+.contain{
+    margin: 3.5vw 0;
+}
+.flow{
+    width: 90%;
+    margin: 3vw auto;
+}
+</style>
--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -6,15 +6,20 @@
 
   <intro></intro>
 
+  <work-flow></work-flow>
+
+  <div class="row" style="height: 500px"></div>
+
 </template>
 
 <script>
 import TheTop from '../components/TheTop.vue';
 import Definition from '../components/Definition.vue';
 import Intro from '../components/Intro.vue';
+import WorkFlow from '../components/WorkFlow.vue';
 
 export default {
-  components: { TheTop, Definition, Intro, },
+  components: { TheTop, Definition, Intro, WorkFlow, },
   
 }
 </script>
