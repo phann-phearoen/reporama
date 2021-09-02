7253dbed127fe216f34869dbed1fc672b129c15d
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-02 15:40:40 +0700

    finish Writters section

--- src/components/intro/Pros.vue
+++ src/components/intro/Pros.vue
@@ -1,64 +1,64 @@
 <template>
+    <div class="contain">
+        <base-title :title="'レポラマにお願いするメリット'"></base-title>
 
-    <base-title :title="'レポラマにお願いするメリット'"></base-title>
+        <div class="row q-mt-lg">
+            <div class="col-1 q-ml-lg"></div>
+            <div class="col-3 self-center pillar">
+                <q-img src="../../assets/merit1.png" width="100%"></q-img>
+            </div>
+            <div class="col-1 self-center pillar">
+                <q-img src="../../assets/arrow1.png"></q-img>
+            </div>
+            <div class="col-6 self-center pillar text">
+                独自性の強いオンリーワンコンテンツができる︕
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-1 q-ml-lg"></div>
+            <div class="col-3 self-center pillar">
+                <q-img src="../../assets/merit1.png"></q-img>
+            </div>
+            <div class="col-1 self-center pillar">
+                <q-img src="../../assets/arrow1.png"></q-img>
+            </div>
+            <div class="col-6 self-center pillar text">
+                独自性の強いオンリーワンコンテンツができる︕
+            </div>
+        </div>
 
-    <div class="row q-mt-lg">
-        <div class="col-1 q-ml-lg"></div>
-        <div class="col-3 self-center pillar">
-            <q-img src="../../assets/merit1.png" width="100%"></q-img>
+        <div class="row">
+            <div class="col-1 q-ml-lg"></div>
+            <div class="col-3 self-center pillar">
+                <q-img src="../../assets/merit1.png"></q-img>
+            </div>
+            <div class="col-1 self-center pillar">
+                <q-img src="../../assets/arrow1.png"></q-img>
+            </div>
+            <div class="col-6 self-center pillar text">
+                独自性の強いオンリーワンコンテンツができる︕
+            </div>
         </div>
-        <div class="col-1 self-center pillar">
-            <q-img src="../../assets/arrow1.png"></q-img>
-        </div>
-        <div class="col-6 self-center pillar text">
-            独自性の強いオンリーワンコンテンツができる︕
+
+        <div class="row q-mt-md">
+            <div class="col column items-center text q-mt-lg">
+                <div>
+                    「大手メディア」の必須コンテンツでもある「体験レポート記事」。
+                </div>
+                <div>
+                    最近は過剰な表現をセーブする方法が主流となり、
+                </div>
+                <div>
+                    PR文も口コミも、さじ加減に悩むメディアが増えています。
+                </div>
+                <div>
+                    等身大の主婦・ママたちが自然体の意見を綴る専門チーム「レポラマ」なら安心。ぜひお任せを︕
+                </div>
+
+            </div>
         </div>
     </div>
-    
-    <div class="row">
-        <div class="col-1 q-ml-lg"></div>
-        <div class="col-3 self-center pillar">
-            <q-img src="../../assets/merit1.png"></q-img>
-        </div>
-        <div class="col-1 self-center pillar">
-            <q-img src="../../assets/arrow1.png"></q-img>
-        </div>
-        <div class="col-6 self-center pillar text">
-            独自性の強いオンリーワンコンテンツができる︕
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-1 q-ml-lg"></div>
-        <div class="col-3 self-center pillar">
-            <q-img src="../../assets/merit1.png"></q-img>
-        </div>
-        <div class="col-1 self-center pillar">
-            <q-img src="../../assets/arrow1.png"></q-img>
-        </div>
-        <div class="col-6 self-center pillar text">
-            独自性の強いオンリーワンコンテンツができる︕
-        </div>
-    </div>
-
-    <div class="row q-mt-lg">
-        <div class="col column items-center text q-mt-lg">
-            <div>
-                「大手メディア」の必須コンテンツでもある「体験レポート記事」。
-            </div>
-            <div>
-                最近は過剰な表現をセーブする方法が主流となり、
-            </div>
-            <div>
-                PR文も口コミも、さじ加減に悩むメディアが増えています。
-            </div>
-            <div>
-                等身大の主婦・ママたちが自然体の意見を綴る専門チーム「レポラマ」なら安心。ぜひお任せを︕
-            </div>
-
-        </div>
-    </div>
-
 </template>
 
 <script>
@@ -77,5 +77,8 @@ export default {
     font-size: 1.7vw;
     color: grey;
     font-weight: 500;
+}
+.contain{
+    margin-bottom: 4vw;
 }
 </style>
