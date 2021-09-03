2f167bfaf0886088fc634040dd6fadf4d9dd0d7a
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-03 15:52:00 +0700

    add contact form content

--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -12,6 +12,8 @@
 
   <faq></faq>
 
+  <contact></contact>
+
   <div class="row" style="height: 500px"></div>
 
 </template>
@@ -23,9 +25,10 @@ import Intro from '../components/Intro.vue';
 import WorkFlow from '../components/WorkFlow.vue';
 import Price from '../components/Price.vue';
 import faq from '../components/FAQ.vue';
+import Contact from '../components/Contact.vue';
 
 export default {
-  components: { TheTop, Definition, Intro, WorkFlow, Price, faq, },
+  components: { TheTop, Definition, Intro, WorkFlow, Price, faq, Contact, },
   
 }
 </script>
