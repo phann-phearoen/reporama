44fa614096e49a1a0032fcf48f291bbbd59a118c
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-06 14:08:03 +0700

    enhance responsibility of BaseTitle

--- src/components/base/BaseTitle.vue
+++ src/components/base/BaseTitle.vue
@@ -1,14 +1,64 @@
 <template>
     <div class="row">
-        <div class="pinkey"><hr class="pinky-line"></div>
-        <slot></slot><div class="title">{{ title }}</div>
-        <div class="pinkey"><hr class="pinky-line"></div>
+        <div class="pinkey"><hr class="pinky-line" :style="{ height: pinkyHeight + 'px', marginTop: pinkyMarginTop + 'vw'}"></div>
+        <slot></slot><div class="title" :style="{ fontSize: titleFontSize + 'vw' }">{{ title }}</div>
+        <div class="pinkey"><hr class="pinky-line" :style="{ height: pinkyHeight + 'px', marginTop: pinkyMarginTop + 'vw'}"></div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['title', ]
+    props: ['title', ],
+    data() {
+        return {
+            screenWidth: 0, 
+            pinkyHeight: 0,
+            titleFontSize: 0,
+            pinkyMarginTop: 0,
+        }
+    },
+    methods: {
+        handleResize() {
+        this.screenWidth = window.innerWidth;
+        },
+    },
+    watch: {
+        screenWidth(val) {
+        if (val > 1600) {
+            this.pinkyHeight = 3;
+            this.titleFontSize = 2;
+            this.pinkyMarginTop = 1.5;
+        } else if (val > 1400) {
+            this.pinkyHeight = 3;
+            this.titleFontSize = 2;
+            this.pinkyMarginTop = 1.5;
+        } else if(val > 1200){ 
+            this.pinkyHeight = 2.5;
+            this.titleFontSize = 2;
+            this.pinkyMarginTop = 1.5;
+        } else if(val > 1000) {
+            this.pinkyHeight = 2;
+            this.titleFontSize = 2;
+            this.pinkyMarginTop = 1.5;
+        } else if(val > 500){
+            this.pinkyHeight = 2;
+            this.titleFontSize = 2;
+            this.pinkyMarginTop = 1.5;
+        } else {
+            this.pinkyHeight = 1.5;
+            this.titleFontSize = 4.5;
+            this.pinkyMarginTop = 3;
+        }
+        },
+    },
+
+    created() {
+        window.addEventListener("resize", this.handleResize);
+        this.handleResize();
+    },
+    unmounted() {
+        window.removeEventListener("resize", this.handleResize);
+    },
 }
 </script>
 
@@ -18,18 +68,15 @@ export default {
     margin: 2vw auto;
 }
 .pinky-line{
-    height: 3px;
     background-color: rgb(205, 75, 128);
     border: none;
     width: 90%;
-    margin-top: 1.5vw;
 }
 .pinkey{
     flex-grow: 1;
 }
 .title{
     margin: 0 auto;
-    font-size: 2.3vw;
     color: rgb(205, 75, 128);
     font-weight: 600;
 }
---
+++ src/pages/screenWidthTemplate.js
@@ -0,0 +1,37 @@
+export default{
+data() {
+    return {
+        screenWidth: 0, 
+    }
+},
+methods: {
+    handleResize() {
+    this.screenWidth = window.innerWidth;
+    },
+},
+watch: {
+    screenWidth(val) {
+    if (val > 1600) {
+
+    } else if (val > 1400) {
+       
+    } else if(val > 1200){ 
+
+    } else if(val > 1000) {
+
+    } else if(val > 500){
+
+    } else {
+
+    }
+    },
+},
+
+created() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+},
+unmounted() {
+    window.removeEventListener("resize", this.handleResize);
+},
+}
