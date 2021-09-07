9793f44bd8aae36ed3e9126e8626ef862e923abd
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-07 13:25:24 +0700

    enhance Others Mobile mode

--- src/components/price/Others.vue
+++ src/components/price/Others.vue
@@ -2,106 +2,102 @@
 <div class="contain">
 
     <div class="row">
-        <base-badge :label="'その他'" :color="'rgb(205, 75, 128)'" :width="'80%'" :pad="'1.4vw'"></base-badge>
+        <base-badge :label="'その他'" :color="'rgb(205, 75, 128)'" :width="'80%'"></base-badge>
     </div>
 
     <base-title :title="'NG商品一覧'"></base-title>
 
-    <div class="row others">
+    <div class="row others" :style="{ width: tableWidth + '%'}">
         <div class="col">
-            <div class="row pinkish-text">
+            <div class="row pinkish-text" :style="{ fontSize: plainTextSize + 'vw' }">
                 以下の商品は取り扱い不可です。
             </div>
-            <div class="row q-mt-md">
-                <div class="col-3 q-mr-md">
+            <div class="row q-mt-sm q-gutter-sm">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-5 grow">
                     <q-card bordered>
                        <q-card-section>
                            <span>                       
-                                <q-img src="../../assets/cross_thick.png" class="cross" width="20%"></q-img>
+                                <q-img src="../../assets/cross_thick.png" class="cross" :style="{ width: crossWidth + 'vw'}"></q-img>
                             </span>
                             <span>                        
-                                <div class="ban">アダルト系</div>                        
+                                <div class="ban" :style="{ fontSize: plainTextSize + 'vw' }">アダルト系</div>                        
                             </span>
                        </q-card-section>
                     </q-card>
                 </div>                
-                <div class="col-3 q-mr-md">
+                <div class="col-xl-2 col-lg-2 col-md-3 col-sm-3 col-xs-5 grow">
                     <q-card bordered>
                        <q-card-section>
                            <span>                       
-                                <q-img src="../../assets/cross_thick.png" class="cross" width="20%"></q-img>
+                                <q-img src="../../assets/cross_thick.png" class="cross" :style="{ width: crossWidth + 'vw'}"></q-img>
                             </span>
                             <span>                        
-                                <div class="ban">暴力系</div>                        
+                                <div class="ban" :style="{ fontSize: plainTextSize + 'vw' }">暴力系</div>                        
                             </span>
                        </q-card-section>
                     </q-card>
                 </div>
-                <div class="col-2 q-mr-md">
+                <div class="col-xl-2 col-lg-2 col-md-3 col-sm-3 col-xs-5 grow">
                     <q-card bordered>
                        <q-card-section>
                            <span>                       
-                                <q-img src="../../assets/cross_thick.png" class="cross"></q-img>
+                                <q-img src="../../assets/cross_thick.png" class="cross" :style="{ width: crossWidth + 'vw'}"></q-img>
                             </span>
                             <span>                        
-                                <div class="ban">海外系</div>                        
+                                <div class="ban" :style="{ fontSize: plainTextSize + 'vw' }">海外系</div>                        
                             </span>
                        </q-card-section>
                     </q-card>
                 </div>
-                <div class="col">
+                <div class="col-xl-3 col-lg-4 col-md-4 col-sm-4 col-xs-5 grow">
                     <q-card bordered>
                        <q-card-section>
                            <span>                       
-                                <q-img src="../../assets/cross_thick.png" class="cross"></q-img>
+                                <q-img src="../../assets/cross_thick.png" class="cross" :style="{ width: crossWidth + 'vw'}"></q-img>
                             </span>
                             <span>                        
-                                <div class="ban">公序良俗に反するモノ</div>                        
+                                <div class="ban" :style="{ fontSize: plainTextSize + 'vw' }">公序良俗に反するモノ</div>                        
                             </span>
                        </q-card-section>
                     </q-card>
                 </div>
-            </div>
-            <div class="row q-mt-md">
-               <div class="col-3 q-mr-md">
+            
+               <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-5 grow">
                     <q-card bordered>
                        <q-card-section>
                            <span>                       
-                                <q-img src="../../assets/cross_thick.png" class="cross" width="20%"></q-img>
+                                <q-img src="../../assets/cross_thick.png" class="cross" :style="{ width: crossWidth + 'vw'}"></q-img>
                             </span>
                             <span>                        
-                                <div class="ban">ギャンブル系</div>                        
+                                <div class="ban" :style="{ fontSize: plainTextSize + 'vw' }">ギャンブル系</div>                        
                             </span>
                        </q-card-section>
                     </q-card>
                 </div>                
-                <div class="col-3 q-mr-md">
+                <div class="col-xl-2 col-lg-2 col-md-3 col-sm-3 col-xs-5 grow">
                     <q-card bordered>
                        <q-card-section>
                            <span>                       
-                                <q-img src="../../assets/cross_thick.png" class="cross" width="20%"></q-img>
+                                <q-img src="../../assets/cross_thick.png" class="cross" :style="{ width: crossWidth + 'vw'}"></q-img>
                             </span>
                             <span>                        
-                                <div class="ban">出会い系</div>                        
+                                <div class="ban" :style="{ fontSize: plainTextSize + 'vw' }">出会い系</div>                        
                             </span>
                        </q-card-section>
                     </q-card>
                 </div>
-                <div class="col">
-                    <q-card bordered>
+                <div class="col-6 grow">
+                    <q-card bordered class="">
                        <q-card-section>
                            <span>                       
-                                <q-img src="../../assets/cross_thick.png" class="cross"></q-img>
+                                <q-img src="../../assets/cross_thick.png" class="cross" :style="{ width: crossWidth + 'vw'}"></q-img>
                             </span>
                             <span>                        
-                                <div class="ban">著作権および法律に抵触するモノ</div>                        
+                                <div class="ban" :style="{ fontSize: plainTextSize + 'vw' }">著作権および法律に抵触するモノ</div>                        
                             </span>
                        </q-card-section>
                     </q-card>
                 </div>
-            </div>
-            <div class="row">
-
             </div>
         </div>
     </div>
@@ -114,7 +110,53 @@ import BaseBadge from '../base/BaseBadge.vue';
 import BaseTitle from '../base/BaseTitle.vue';
 
 export default {
-    components: { BaseBadge, BaseTitle, },    
+    components: { BaseBadge, BaseTitle, },   
+        data() {
+            return {
+                screenWidth: 0, 
+                tableWidth: 0,
+                plainTextSize: 0,
+                crossWidth: 0,
+            }
+    },
+    methods: {
+        handleResize() {
+        this.screenWidth = window.innerWidth;
+        },
+    },
+    watch: {
+        screenWidth(val) {
+            if (val > 1600) {
+                this.tableWidth = 75
+                this.plainTextSize = 1.3
+                this.crossWidth = 3
+            } else if (val > 1400) {
+            
+            } else if(val > 1200){ 
+
+            } else if(val > 1000) {
+                this.tableWidth = 85
+                this.plainTextSize = 2
+                this.crossWidth = 3
+            } else if(val > 500){
+                this.tableWidth = 90
+                this.plainTextSize = 2.6
+                this.crossWidth = 3.5
+            } else {
+                this.tableWidth = 90
+                this.plainTextSize = 2.9
+                this.crossWidth = 5
+            }
+        },
+    },
+
+    created() {
+        window.addEventListener("resize", this.handleResize);
+        this.handleResize();
+    },
+    unmounted() {
+        window.removeEventListener("resize", this.handleResize);
+    }, 
 }
 </script>
 
@@ -123,25 +165,21 @@ export default {
     margin: 3.5vw 0;
 }
 .others{ 
-    width: 70%;
     margin: 1vw auto;
 }
 .pinkish-text{
-    font-size: 1.5vw;
     color: rgb(205, 75, 128);
 }
 
 .ban{
-    font-size: 1.3vw;
     color: grey;
     display: inline-block;
     margin-top: 1vw;
 }
 .cross{
-    margin-bottom: .7vw;
-    min-width: 3vw;
-    min-height: 3vw;
-    height: 3vw;
-    width: 3vw;
+    
+}
+.grow{
+    flex-grow: 1;
 }
 </style>
