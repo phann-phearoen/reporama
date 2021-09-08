dda565b2be60f6f90f3d2046fa5858734e460040
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-08 10:36:11 +0700

    add validation functions to all inputs

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,46 +5,49 @@
     </div>
 
     <q-form
-    @submit="onSubmit"
+    @submit.prevent.stop="onSubmit"
     class="form" :style="{ width: formWidth + '%' }"
     >
         <q-input class="input"
+            ref="nameRef"
             outlined
             v-model="name"
             label="お名前"
             hint="必須"
-            lazy-rules
-            :rules="[ val => val && val.length > 0 || 'お名前を入力してください。']"
+            :rules="validateName"
         />
 
         <q-input class="input"
+            ref="comRef"
             outlined
             v-model="company"
             label="会社名"
             hint="必須"
-            lazy-rules
-            :rules="[ val => val && val.length > 0 || '会社名を入力してください。']"
+            :rules="validateCompany"
         />
 
         <q-input class="input"
+            ref="emailRef"
             outlined
             v-model="email"
             label="メールアドレス"
             hint="必須"
-            lazy-rules
-            :rules="[ val => val && val.length > 0 || 'メールアドレスを入力してください。']"
+            :rules="validateEmail"
         />
 
         <q-input class="input"
+            ref="phoneRef"
             outlined
             v-model="phone"
-            type="tel"
             label="電話番号"
+            mask="###-####-####"
+            :rules="validatePhone"
         />
 
         <div class="plan input">
             <div class="label">
-                <span>お申込みプラン</span><span>（必須）</span>
+                <span>お申込みプラン</span><span v-if="appPlanIsValid==null">（必須）</span>
+                <span style="color: red" v-if="appPlanIsValid==false">（必須）</span>
             </div>
             <hr>
             <div class="row">
@@ -82,7 +85,7 @@
                 <div class="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-xs-11 q-mt-sm" style="display: inline-block">
                     <q-input
                         outlined
-                        v-model="articleCount.choice1"
+                        v-model="articleCount.choice2"
                         type="number"
                         suffix="本"
                     />
@@ -103,7 +106,7 @@
                 <div class="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-xs-11 q-mt-sm" style="display: inline-block">
                     <q-input
                         outlined
-                        v-model="articleCount.choice1"
+                        v-model="articleCount.choice3"
                         type="number"
                         suffix="本"
                     />
@@ -114,7 +117,8 @@
 
         <div class="publish input">
             <div class="label">
-                <span>salvia への掲載を希望しますか︖</span><span>（必須）</span>
+                <span>salvia への掲載を希望しますか︖</span><span v-if="postConsentsIsValid==null">（必須）</span>
+                <span style="color: red" v-if="postConsentsIsValid==false">（必須）</span>
             </div>
             <hr>
             <div class="row">
@@ -133,7 +137,8 @@
 
         <div class="kikaku input">
             <div class="label">
-                <span>「salvia 読者プレゼントキャンペーン」での企画を希望しますか︖</span><span>（必須）</span>
+                <span>「salvia 読者プレゼントキャンペーン」での企画を希望しますか︖</span><span v-if="kikakuConsentsIsValid==null">（必須）</span>
+                <span style="color: red" v-if="kikakuConsentsIsValid==false">（必須）</span>
             </div>
             <hr>
             <div class="row">
@@ -147,14 +152,14 @@
         </div>
 
         <q-input
+            ref="inqRef"
             class="input"
             outlined
             type="textarea"
             v-model="inquiry"
             label="お問い合わせの内容"
             hint="必須"
-            lazy-rules
-            :rules="[ val => val && val.length > 0 || 'お問い合わせの内容を入力してください。']"
+            :rules="validateInquiry"
         />
 
         <div class="row">
@@ -179,31 +184,74 @@ export default {
     data() {
         return {
             name: '',
+            validateName: [ val => val && val.length > 0 || 'お名前を入力してください。' ],
+
             company: '',
-            email: '',
-            phone: '',
+            validateCompany: [ val => val && val.length > 0 || '会社名を入力してください。' ],
+
+            email: null,
+            validateEmail: [
+                val => (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(val)) || '正しいアドレスを入力してください。'
+            ],
+
+            phone: null,
+            validatePhone: [
+                val => (val.match(/^0\d{1,4}-\d{1,4}-\d{3,4}$/)) || '正しい電話番号を入力してください。'
+            ],
+
             wordCount: null,
             articleCount: {
                 choice1: null, choice2: null, choice3: null
             },
+            appPlanIsValid: null,
+
             postConsents: null,
+            postConsentsIsValid: null,
+
             kikakuConsents: null,
+            kikakuConsentsIsValid: null,
+
             inquiry: '',
+            validateInquiry: [ val => val && val.length > 0 || 'お問い合わせ内容を入力してください。'],
+            
             screenWidth: 0, 
             formWidth: 0,
             toTopWidth: 0,
             toTopMarginRight: null,
+
         }
     },
+    computed: {
+    },
     methods: {
-        onSubmit() {
-            
-        },
         toTop() {
             window.scrollTo({ top: 0, left: 0, behavior: 'smooth', });
         },
         handleResize() {
             this.screenWidth = window.innerWidth;
+        },
+
+        validateAppPlan() {
+            if (this.wordCount && (this.articleCount.choice1 || this.articleCount.choice2 || this.articleCount.choice3))
+                this.appPlanIsValid = true;
+            else
+                this.appPlanIsValid = false;
+        },
+        validatePostConsents() {
+            if (this.postConsents)
+                this.postConsentsIsValid = true;
+            else
+                this.postConsentsIsValid = false;
+        },
+        validateKikakuConsents() {
+            if (this.kikakuConsents)
+                this.kikakuConsentsIsValid = true;
+            else
+                this.kikakuConsentsIsValid = false;
+        },
+        onSubmit() {
+            
+            
         },
     },
     watch: {
